318b2a04131a5cea5f6dd2c082c197c568d39355
Santhoshkumar044 <santhoshkumar.vt9a@gmail.com>
2025-07-12 13:20:00 +0530

    Login and sign up module completed

---
+++ models/User.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  walletAddress: { type: String, required: true },
+  userId: { type: String, unique: true, required: true }
+},);
+
+export default mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"
 import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -8,13 +10,15 @@ const app = express();
 const PORT = process.env.PORT || 5070;
 
 connectDB();
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("🚀 Server is running and DB is connected!");
 });
+app.use("/api/auth",authRoute);
 
 app.listen(PORT, () => {
   console.log(`🌐 Server running on http://localhost:${PORT}`);
+console.log("Loaded JWT secret:", process.env.JWT_SECRET);
 });
